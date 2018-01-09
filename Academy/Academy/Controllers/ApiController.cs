f78e764b1df59b48e1697e57a860fbaa0dce35d9
Kero76 <nic.gille@gmail.com>
2018-01-09 15:48:27 +0100

    :pencil: Add academies list on AddOrUpdate view.

---
+++ Academy/Academy/Controllers/ApiController.cs
@@ -0,0 +1,28 @@
+﻿using Academy.Models;
+using Academy.Repositories;
+using Academy.WebModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Academy.Controllers
+{
+    public class APIController : Controller
+    {
+        public AcademyRepository AcademyRepository { get; set; }
+
+        public APIController()
+        {
+            AcademyRepository = new AcademyRepository(new Entities());
+        }
+
+        public ActionResult GetAcademies()
+        {
+            var academies = AcademyRepository.All().Select(a => AcademyModel.ToModel(a));
+            return Json(academies, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
