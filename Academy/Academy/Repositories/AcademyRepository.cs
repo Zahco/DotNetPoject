b08137c0163ac4ea8c9e6ca0c6678fd1ea405a5c
Nicolas GILLE <nic.gille@gmail.com>
2018-02-06 16:53:14 +0100

    :pencil: Complete CRUD for Academy

--- Academy/Academy/Repositories/AcademyRepository.cs
+++ Academy/Academy/Repositories/AcademyRepository.cs
@@ -8,11 +8,26 @@ namespace Academy.Repositories
 {
     public class AcademyRepository : Repository<Academies>
     {
-        public AcademyRepository(Entities.Entities _dbase) : base(_dbase.Academies, _dbase) { }
+        private EstablishmentRepository establishmentRepository;
+
+        public AcademyRepository(Entities.Entities _dbase) : base(_dbase.Academies, _dbase)
+        {
+            establishmentRepository = new EstablishmentRepository(_dbase);
+        }
 
         public Academies GetByName(string name)
         {
             return All().FirstOrDefault(a => a.Name == name);
+        }
+
+        protected override void BeforeDelete(Academies entity)
+        {
+            // Copy the lisf of Establishment to avoid problem during process.
+            var list = entity.Establishments.ToList();
+            foreach (var e in list)
+            {
+                establishmentRepository.Delete(e.Id);
+            }
         }
     }
 }
