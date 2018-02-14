516254312cbc2c8fa9c4cc2a3c94655d03c70a21
Unknown <geoffrey.spaur@gmail.com>
2018-02-14 16:34:54 +0100

    CRUD - Periods

---
+++ Academy/Academy/Controllers/PeriodController.cs
@@ -0,0 +1,82 @@
+﻿using Academy.Entities;
+using Academy.Models;
+using Academy.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Academy.Controllers
+{
+    public class PeriodController : Controller
+    {
+        public PeriodRepository PeriodRepository { get; set; }
+
+        public PeriodController()
+        {
+            var entities = new Entities.Entities();
+            PeriodRepository = new PeriodRepository(entities);
+        }
+
+        public ActionResult GetAll()
+        {
+            return View(PeriodRepository.All().Select(p => PeriodModel.ToModel(p)));
+        }
+
+        public ActionResult Get(Guid id)
+        {
+            return View(PeriodModel.ToModel(PeriodRepository.GetById(id)));
+        }
+
+        public ActionResult AddOrUpdate(Guid? id)
+        {
+            var model = new PeriodModel();
+            if (id.HasValue)
+            {
+                model = PeriodModel.ToModel(PeriodRepository.GetById(id.Value));
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrUpdate(PeriodModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var isCreated = model.Id == Guid.Empty;
+            var period = new Periods();
+            if (!isCreated)
+            {
+                period = PeriodRepository.GetById(model.Id);
+            }
+
+            period.Begin = model.Begin;
+            period.End = model.End;
+            period.Year_Id = model.Year.Id;
+
+            if (isCreated)
+            {
+                PeriodRepository.Add(period);
+            }
+            PeriodRepository.Save();
+
+            return Redirect(Url.Action("Get", "Period", new { id = period.Id }));
+        }
+
+        public ActionResult Delete(Guid Id)
+        {
+            PeriodRepository.Delete(Id);
+            PeriodRepository.Save();
+            return Redirect(Url.Action("GetAll", "Period"));
+        }
+
+        public ActionResult GetByFilter(string filter)
+        {
+            return Json(PeriodRepository.All()
+                .Where(p => p.Years.Year.ToString().ToUpper().Contains(filter.ToUpper()))
+                .Select(p => p.Id), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
