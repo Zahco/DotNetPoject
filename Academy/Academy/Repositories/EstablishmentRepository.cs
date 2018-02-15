08ea30a0c7670dba3b2a3daa7d77bb9286f06ac8
Nicolas GILLE <nic.gille@gmail.com>
2018-02-15 14:01:33 +0100

    :pencil: Add deletion in cascade for Establishment, Evaluation and Pupil.

--- Academy/Academy/Repositories/EstablishmentRepository.cs
+++ Academy/Academy/Repositories/EstablishmentRepository.cs
@@ -8,10 +8,25 @@ namespace Academy.Repositories
 {
     public class EstablishmentRepository : Repository<Establishments>
     {
-        public EstablishmentRepository(Entities.Entities _dbase) : base(_dbase.Establishments, _dbase) { }
+        private ClassroomRepository classroomRepository;
+
+        public EstablishmentRepository(Entities.Entities _dbase) : base(_dbase.Establishments, _dbase)
+        {
+            classroomRepository = new ClassroomRepository(_dbase);
+        }
         public Establishments GetByName(string name)
         {
             return All().FirstOrDefault(a => a.Name == name);
+        }
+
+        protected override void BeforeDelete(Establishments entity)
+        {
+            // Copy the lisf of Classroom to avoid problem during process.
+            var list = entity.Classrooms.ToList();
+            foreach (var c in list)
+            {
+                classroomRepository.Delete(c.Id);
+            }
         }
     }
 }
--- Academy/Academy/Repositories/EvaluationRepository.cs
+++ Academy/Academy/Repositories/EvaluationRepository.cs
@@ -9,6 +9,21 @@ namespace Academy.Repositories
 {
     public class EvaluationRepository : Repository<Evaluations>
     {
-        public EvaluationRepository(Entities.Entities _dbase) : base(_dbase.Evaluations, _dbase) { }
+        private ResultRepository resultRepository;
+
+        public EvaluationRepository(Entities.Entities _dbase) : base(_dbase.Evaluations, _dbase)
+        {
+            resultRepository = new ResultRepository(_dbase);
+        }
+
+        protected override void BeforeDelete(Evaluations entity)
+        {
+            // Copy the lisf of Result to avoid problem during process.
+            var list = entity.Results.ToList();
+            foreach (var r in list)
+            {
+                resultRepository.Delete(r.Id);
+            }
+        }
     }
 }
